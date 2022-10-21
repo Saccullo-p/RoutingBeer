a6137863f29cc5a17b02967059efa76e41d6060a
Saccullo-p <saccullo.pietro@iisgalvanimi.edu.it>
2022-10-21 10:57:39 +0000

    punto4.1

--- routing-beer/src/app/item-birre/item-birre.component.ts
+++ routing-beer/src/app/item-birre/item-birre.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { BirreService } from '../birre.service';
 
 @Component({
-  selector: 'app-item-birre',
+  selector: 'app-item',
   templateUrl: './item-birre.component.html',
   styleUrls: ['./item-birre.component.css']
 })
 export class ItemBirreComponent implements OnInit {
-
-  constructor() { }
+  routeObs: Observable<ParamMap> | undefined;
+  foodServiceObs: Observable<Object> | undefined;
+  items: any;
+  constructor(
+  private route: ActivatedRoute,
+  private router: Router,
+  private service: BirreService) {}
 
   ngOnInit(): void {
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
   }
-
+  getRouterParam = (params: ParamMap) => {
+    console.log(params);
+    let itemId = params.get('id'); //Ottengo l'id dai parametri
+    console.log(itemId); //Stampo su console
+    this.foodServiceObs = this.service.searchD(itemId!);
+    this.foodServiceObs.subscribe((data) => ((this.items = data), console.log(data)));
+  };
 }
